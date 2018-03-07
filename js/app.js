802c02a8cd517084f0a57e19b96381e3ac791cb6
abigail <alex.abi.roja@gmail.com>
2018-03-07 02:10:41 -0600

    avance

--- js/app.js
+++ js/app.js
@@ -1,66 +1,37 @@
-/*/hacer la peticion de la api
-const firstAjax = () => {
-  const url = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/699f93a24e710183b485997925cf150d/37.8267,-122.4233"
-  fetch(url)
-       .then( response => response.json()).then( json => drawWeather(json));
+
+//hacer la peticion de la api y al final sevuelve allamar esta funcion para que corra el programa.
+const peticion = () => {
+
+   fetch( "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/699f93a24e710183b485997925cf150d/37.8267,-122.4233")
+    .then ( response => response.json()).then(json => dataClima(json));// es convertir en json los datos de la api
   };
 
-  const drawWeather = function(json) {
-      console.log (json);
-      const weatherTodayContainer = document.getElementById('weather-today-container')
-      const weatherForecastContainer = document.getElementById('weather-forecast-container')
-
+  const dataClima = json => {
+    console.log(json);
+//solo se llama a los id done se quiere la informacion
+    const today = document.getElementById("today-container");
+    const next = document.getElementById("next-container");
+//para que se pinte en el html
     let template = `
-        <div>${json.currently.apparentTeperature}</div>
-        <div>${json.currently.humidity}</div>
-        <div>${json.currently.uvIndex}</div>
-        <div>${json.currently.pressure}`;
-    weatherTodayContainer.innerHTML =  template;
+    <div> Temperature: ${json.currently.apparentTemperature}</div>
+    <div> Humidity: ${json.currently.humidity}</div>
+    <div> UV index: ${json.currently.uvIndex}</div>
+    <div> Presurre: ${json.currently.pressure} `;
+  today.innerHTML=template;
 
-    let templateForcast = json.daily.data.forEach(function(day) {
-      console.log(day);
-
-      let currentDay = `
-      <div>Icon :${day.icon}</div>
-      <div> Temperature-high: ${day.temperatureHigh} and temperature-min ${day.temperatureMin}`;
-    weatherForecastContainer = template;
-
+  //este es para pintar los pronosticos de lo dias siguientes
+let templateForcast = json.daily.data.forEach( day => {// se va recorriendo el array
+  //para sacar la fecha actual, y la temperatura min
+        let currentDay = `<hr>
+            <div>${currentDate(day.time)}</div>
+           <div>Icon: ${day.icon}</div>
+           <div>Temperature-high: ${day.temperatureHigh} and Temperature-min: ${day.temperatureMin}</>`;
+        //se aplican metodos nuevos
+        next.insertAdjacentHTML('beforeEnd', currentDay);
   })
 
-
-
 };
+//esto es para que se ponga en el horario normal
+let currentDate =(unixNumber) => new Date (unixNumber*1000).toLocaleString('es-MX',{weekday: 'long'});
 
-firstAjax();
-*/
-//hacer la peticion de la api
-fetch( "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/699f93a24e710183b485997925cf150d/37.8267,-122.4233"
-).then(function(response){
-  //console.log(response);
-   return response.json();
- })
-    .then(function(dataClima){
-     console.log(dataClima);
-});
-
-const dataClima = (function(json){
-  let
-
-})
-
-
-
-  /*
-  //infoClima(dataClima);
-})
-   .catch(function(error){
-  //  console.log('existe un problema');
-});
-
-let infoClima = (dataClima) => {
-
-    dataClima.forEach(function(element){
-        let wind = element.wind;
-        //console.log(wind);
-    })
-};*/
+peticion();
